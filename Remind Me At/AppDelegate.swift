d872e97ab6c0f5ad3f791ba2c50f2a8903f39785
Rishabh <rishabh@Abhinavs-Mac.local>
2017-09-25 19:21:35 +0530

    updated

--- Remind Me At/AppDelegate.swift
+++ Remind Me At/AppDelegate.swift
@@ -14,6 +14,7 @@ import UserNotifications
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+   
     var window: UIWindow?
     
     let coreDataManager = CoreDataManager.sharedInstance
@@ -21,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // facebook
+        // facebook//
         FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         //Request notification authorisation
